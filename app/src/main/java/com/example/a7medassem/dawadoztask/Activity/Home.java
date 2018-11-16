1bfca018488a9015b3dc8d3e05a5455dacd3d092
ahmed3ssem <a7med.assem@gmail.com>
2018-11-16 22:25:51 +0200

    "Add Contact Us"

--- app/src/main/java/com/example/a7medassem/dawadoztask/Activity/Home.java
+++ app/src/main/java/com/example/a7medassem/dawadoztask/Activity/Home.java
@@ -1,7 +1,12 @@
 package com.example.a7medassem.dawadoztask.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.a7medassem.dawadoztask.R;
 
@@ -11,5 +16,24 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contactus:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto","Dev@dawadoz.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "DawadozTask");
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
